b59ca950eca68d6b54926db6d627be00f90af794
Jessica <jessicakchell@gmail.com>
2022-05-30 01:40:56 +0200

    confirmation page

--- components/confirmation/ConfirmationElements.js
+++ components/confirmation/ConfirmationElements.js
@@ -29,4 +29,43 @@ const ConfirmationHeroTitle = styled.h1`
     `}
 `;
 
-export { ConfirmationHeroContainer, ConfirmationHeroTitle };
+const ConfirmationContentContainer = styled.div`
+	padding: 100px 100px 0 100px;
+
+	${media.lessThan('medium')`
+		padding: 100px 25px;
+	`}
+
+	h5 {
+		font-size: var(--h5-size);
+		font-weight: var(--font-weight-reg);
+		text-transform: uppercase;
+		letter-spacing: var(--letter-spacing-md);
+		line-height: var(--line-height-md);
+	}
+`;
+
+const Signature = styled.div`
+	h3 {
+		font-family: var(--font-secondary);
+		font-size: var(--h3-size);
+		font-weight: var(--font-weight-reg);
+	}
+`;
+
+const ContactLink = styled.a`
+	color: var(--color-black);
+	text-decoration: underline;
+
+	:hover {
+		cursor: pointer;
+	}
+`;
+
+export {
+	ConfirmationHeroContainer,
+	ConfirmationHeroTitle,
+	ConfirmationContentContainer,
+	Signature,
+	ContactLink,
+};
--- pages/confirmation.js
+++ pages/confirmation.js
@@ -1,8 +1,12 @@
 import Page from '../components/page/Page';
 import {
+	ConfirmationContentContainer,
 	ConfirmationHeroContainer,
 	ConfirmationHeroTitle,
+	Signature,
+	ContactLink,
 } from '../components/confirmation/ConfirmationElements';
+import Link from 'next/link';
 
 function Confirmation() {
 	return (
@@ -11,6 +15,24 @@ function Confirmation() {
 				<ConfirmationHeroContainer>
 					<ConfirmationHeroTitle>See you soon!</ConfirmationHeroTitle>
 				</ConfirmationHeroContainer>
+
+				<ConfirmationContentContainer>
+					<h5>Your Reservation Confirmation</h5>
+					<p className='inner-content-padding-sm-top'>
+						Thank you for choosing to stay at one of our Holidaze locations.
+						Once one of our agents have looked over your enquiry, we will send
+						you a confirmation email with all the details! See you soon. Please
+						don&apos;t hesitate to{' '}
+						<Link href='/contactus'>
+							<ContactLink>contact us</ContactLink>
+						</Link>{' '}
+						if you need to make a change.{' '}
+					</p>
+
+					<Signature className='inner-content-padding-top'>
+						<h3>xoxo Holidaze</h3>
+					</Signature>
+				</ConfirmationContentContainer>
 			</Page>
 		</>
 	);
